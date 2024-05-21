a6297b6904375a0170f1b82efa26b760b77ee0b4
Josue-hernandez <97258271+Josue-HernandezL@users.noreply.github.com>
2024-05-20 21:34:56 -0600

    complete structure

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const d = document;
 const $btnSendRequest = d.getElementById("send-request");
+const $responseIa = d.getElementById("response-ia");
 
 $btnSendRequest.addEventListener("click", (e) => {
     const $requestIa = d.getElementById("request").value;
+    const $txtDemo = d.getElementById("demo");
 
+    e.preventDefault();
     fetch('https://fi.jcaguilar.dev/v1/chat', {
         method: 'POST',
         headers: {
@@ -16,7 +19,13 @@ $btnSendRequest.addEventListener("click", (e) => {
 
     .then(response => response.json())
     .then(data => {
+        $txtDemo.style.display ="none";
+        $responseIa.innerHTML += `
+        <h2>${$requestIa}</h2>
+        <p>${data.text}</p>
+        `
         console.log(data);
+        
     })
     .catch(err => {
         console.log(err);
